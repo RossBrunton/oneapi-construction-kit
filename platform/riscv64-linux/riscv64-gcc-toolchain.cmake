9101ac3dcffc9b5808b5151266f12a27cb6268f3
Ross Brunton <ross@codeplay.com>
2024-02-26 14:11:05 +0000

    Add RISCV64 compiler support

--- platform/riscv64-linux/riscv64-gcc-toolchain.cmake
+++ platform/riscv64-linux/riscv64-gcc-toolchain.cmake
@@ -14,6 +14,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+# Note: By default this looks for tools in `/usr/bin`, which is where the
+# Ubuntu `gcc-*-riscv64-linux-gnu` packages place it.
+# For building OCK, qemu is also required (`qemu-user` in Ubuntu).
+
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR riscv64)
 
@@ -22,17 +26,38 @@ if(CMAKE_HOST_SYSTEM_NAME STREQUAL Windows)
     Windows")
 endif()
 
-set(TOOLCHAIN_ROOT "" CACHE PATH "path to toolchain root directory")
+set(TOOLCHAIN_ROOT "/usr" CACHE PATH "path to toolchain root directory")
 set(CMAKE_TRY_COMPILE_PLATFORM_VARIABLES TOOLCHAIN_ROOT)
 if (NOT TOOLCHAIN_ROOT)
   message(FATAL_ERROR "TOOLCHAIN_ROOT must be defined when using the riscv64 \
     toolchain!")
 endif()
 
-set(TRIPLE riscv64-unknown-linux-gnu)
+set(TRIPLE riscv64-linux-gnu)
 
-set(CMAKE_C_COMPILER "${TOOLCHAIN_ROOT}/bin/${TRIPLE}-gcc" CACHE PATH "gcc" FORCE)
-set(CMAKE_CXX_COMPILER "${TOOLCHAIN_ROOT}/bin/${TRIPLE}-g++" CACHE PATH "g++" FORCE)
+# LLVM requires GCC 7 or up
+find_program(CMAKE_C_COMPILER NAMES
+             "${TOOLCHAIN_TRIPLE}-gcc"
+             "${TOOLCHAIN_TRIPLE}-gcc-13"
+             "${TOOLCHAIN_TRIPLE}-gcc-12"
+             "${TOOLCHAIN_TRIPLE}-gcc-11"
+             "${TOOLCHAIN_TRIPLE}-gcc-10"
+             "${TOOLCHAIN_TRIPLE}-gcc-9"
+             "${TOOLCHAIN_TRIPLE}-gcc-8"
+             "${TOOLCHAIN_TRIPLE}-gcc-7"
+             PATHS "${TOOLCHAIN_ROOT}/bin/" DOC "gcc")
+
+find_program(CMAKE_CXX_COMPILER NAMES
+             "${TOOLCHAIN_TRIPLE}-g++"
+             "${TOOLCHAIN_TRIPLE}-g++-13"
+             "${TOOLCHAIN_TRIPLE}-g++-12"
+             "${TOOLCHAIN_TRIPLE}-g++-11"
+             "${TOOLCHAIN_TRIPLE}-g++-10"
+             "${TOOLCHAIN_TRIPLE}-g++-9"
+             "${TOOLCHAIN_TRIPLE}-g++-8"
+             "${TOOLCHAIN_TRIPLE}-g++-7"
+             PATHS "${TOOLCHAIN_ROOT}/bin/" DOC "g++")
+
 set(CMAKE_AR "${TOOLCHAIN_ROOT}/bin/${TRIPLE}-ar" CACHE PATH "archive" FORCE)
 set(CMAKE_LINKER "${TOOLCHAIN_ROOT}/bin/${TRIPLE}-ld" CACHE PATH "linker" FORCE)
 set(CMAKE_NM "${TOOLCHAIN_ROOT}/bin/${TRIPLE}-nm" CACHE PATH "nm" FORCE)
@@ -40,3 +65,23 @@ set(CMAKE_OBJCOPY "${TOOLCHAIN_ROOT}/bin/${TRIPLE}-objcopy" CACHE PATH "objcopy"
 set(CMAKE_OBJDUMP "${TOOLCHAIN_ROOT}/bin/${TRIPLE}-objdump" CACHE PATH "objdump" FORCE)
 set(CMAKE_STRIP "${TOOLCHAIN_ROOT}/bin/${TRIPLE}-strip" CACHE PATH "strip" FORCE)
 set(CMAKE_RANLIB "${TOOLCHAIN_ROOT}/bin/${TRIPLE}-ranlib" CACHE PATH "ranlib" FORCE)
+
+find_program(QEMU_RISCV64_EXECUTABLE qemu-riscv64)
+if(NOT QEMU_RISCV64_EXECUTABLE MATCHES NOTFOUND)
+  set(CMAKE_CROSSCOMPILING_EMULATOR
+    ${QEMU_RISCV64_EXECUTABLE} -L ${CMAKE_FIND_ROOT_PATH}
+    CACHE STRING "qemu" FORCE)
+endif()
+
+set(TARGET_FLAGS "")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${BASE_FLAGS} " CACHE STRING "c flags" FORCE)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${BASE_FLAGS}" CACHE STRING "c++ flags" FORCE)
+
+set(LINKER_FLAGS "${TARGET_FLAGS}")
+set(LINKER_LIBS "")
+set(CMAKE_SHARED_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
+set(CMAKE_MODULE_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
+
+set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_CXX_COMPILER> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> -o <TARGET>  <OBJECTS> <LINK_LIBRARIES> ${LINKER_LIBS}" CACHE STRING "Linker command line" FORCE)
